279d60fff6b7e999db2dadc5a28761cb45971e9c
Leif Johansson <leifj@sunet.se>
2019-05-05 21:45:55 +0200

    base docker-image on nginx and proxy backend MDQ server

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM debian:stable
 MAINTAINER Leif Johansson <leifj@sunet.se>
-RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-RUN apt-get -q update && apt-get -y upgrade && apt-get -y install lighttpd jq
-COPY dist/ /var/www/html/
+RUN apt-get update -q && apt-get install -yy libnginx-mod-http-lua nginx-extras
+COPY dist/ /usr/share/nginx/html/
+COPY nginx.conf /etc/nginx/nginx.conf
 ADD start.sh /
 RUN chmod a+rx /start.sh
 ENTRYPOINT ["/start.sh"]
--- start.sh
+++ start.sh
@@ -5,14 +5,16 @@
 # to the stdout and stderr, so that `docker logs -f lighthttpd` works.
 
 
-mdq_url=${MDQ_URL:-"https://${HOSTNAME}/entities/"}
+mdq_url=${MDQ_URL:-"http://${MDQ_HOST}:${MDQ_PORT}/entities/"}
 pyff_apis=${PYFF_APIS:-false}
 mdq_servername=${MDQ_SERVERNAME:-""}
 
-jq -n --arg mdq_url "$mdq_url" --arg pyff_apis "$pyff_apis" --arg mdq_servername "$mdq_servername" \
-   '{mdq_url: $mdq_url, mdq_servername: $mdq_servername, pyff_apis: $pyff_apis}' > /var/www/html/config.json
+cat<<EOF>/usr/share/nginx/html/config.json
+{
+   "mdq_url": "$mdq_url",
+   "mdq_servername": "$mdq_servername",
+   "pyff_apis": $pyff_apis
+}
+EOF
 
-
-tail -F /var/log/lighttpd/access.log 2>/dev/null &
-tail -F /var/log/lighttpd/error.log 2>/dev/null 1>&2 &
-lighttpd -D -f /etc/lighttpd/lighttpd.conf
+nginx -c /etc/nginx/nginx.conf

f6ca0e96dd425a613a12b9ed2a2bbfb0dddd15d0
Leif Johansson <leifj@sunet.se>
2019-05-05 21:46:48 +0200

    drop mocker - not used

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,24 +2,11 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require("webpack");
 const path = require('path');
-const apiMocker = require('mocker-api');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
    mode: 'development',
    devtool: 'inline-source-map',
-   devServer: {
-       contentBase: './dist',
-       port: 9000,
-       before(ds) {
-           apiMocker(ds, path.resolve('./mocker/index.js'), {
-               proxy: {
-                   '/entities/*': 'http://localhost:8080/'
-               },
-               changeHost: true
-           })
-       }
-   },
    plugins: [
        //new BundleAnalyzerPlugin(),
        new webpack.EnvironmentPlugin({

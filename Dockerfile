f51c3f8d7ab4e93a98b461901cc25af244bc0443
Leif Johansson <leifj@sunet.se>
2019-05-05 14:30:26 +0200

    fix start script

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM debian:stable
 MAINTAINER Leif Johansson <leifj@sunet.se>
 RUN echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-RUN apt-get -q update && apt-get -y upgrade && apt-get -y install lighttpd
-COPY dist/* /var/www/html/
+RUN apt-get -q update && apt-get -y upgrade && apt-get -y install lighttpd jq
+COPY dist/ /var/www/html/
 ADD start.sh /
 RUN chmod a+rx /start.sh
-ENTRYPOINT ["start.sh"]
+ENTRYPOINT ["/start.sh"]
--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # from https://github.com/spujadas/lighttpd-docker/blob/master/start.sh
 # this launcher script tails the access and error logs
 # to the stdout and stderr, so that `docker logs -f lighthttpd` works.
+
+
+mdq_url=${MDQ_URL:-"https://${HOSTNAME}/entities/"}
+pyff_apis=${PYFF_APIS:-false}
+mdq_servername=${MDQ_SERVERNAME:-""}
+
+jq -n --arg mdq_url "$mdq_url" --arg pyff_apis "$pyff_apis" --arg mdq_servername "$mdq_servername" \
+   '{mdq_url: $mdq_url, mdq_servername: $mdq_servername, pyff_apis: $pyff_apis}' > /var/www/html/config.json
+
 
 tail -F /var/log/lighttpd/access.log 2>/dev/null &
 tail -F /var/log/lighttpd/error.log 2>/dev/null 1>&2 &

61f00f8a95c1f5a6f03ea320d7091024e88b26a4
Leif Johansson <leifj@sunet.se>
2019-05-30 21:05:27 +0200

    remove old docs in start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# from https://github.com/spujadas/lighttpd-docker/blob/master/start.sh
-# this launcher script tails the access and error logs
-# to the stdout and stderr, so that `docker logs -f lighthttpd` works.
-
-
 mdq_url=${MDQ_URL:-"http://${MDQ_HOST}:${MDQ_PORT}/entities/"}
 pyff_apis=${PYFF_APIS:-false}
 mdq_servername=${MDQ_SERVERNAME:-""}
